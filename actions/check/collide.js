d268963c859ccbf22f48df2a28f4ea0e28714e45
xu tao <429499381@qq.com>
2020-03-18 10:27:20 +0800

    做用例测试

--- actions/check/collide.js
+++ actions/check/collide.js
@@ -20,24 +20,27 @@ var collide = function (ball, paddle) {
     var leftY = ball.y
     var rightY = ball.y + (ball.image.height || ball.h)
 
+    paddle.h = paddle.image.height || paddle.h
+    paddle.w = paddle.image.width || paddle.w
+
     // 球的左上点X 检测
     if (leftX > paddle.x && leftX < paddle.x + paddle.w) {
         if (leftY > paddle.y && leftY < paddle.y + paddle.h) {
-            log('x 左上点相撞')
+           // log('x 左上点相撞')
             return true
         }
     }
     // 球的右上点x 检测
     if (rightX > paddle.x && rightX < paddle.x + paddle.w) {
         if (leftY < paddle.y + paddle.h && leftY > paddle.y) {
-            log('x 右上点相撞')
+           // log('x 右上点相撞')
             return true
         }
     }
     // 球的左下点  检测
     if (rightY > paddle.y && rightY < paddle.y + paddle.h) {
         if (leftX > paddle.x && leftX < paddle.x + paddle.w) {
-            log('y 左下点 相撞')
+            //log('y 左下点 相撞')
             return true
         }
     }
@@ -45,7 +48,7 @@ var collide = function (ball, paddle) {
     // 球的右下点 检测
     if (rightY > paddle.y && rightY < paddle.y + paddle.h) {
         if (rightX > paddle.x && rightX < paddle.x + paddle.w) {
-            log('y 右下点相撞')
+            //log('y 右下点相撞')
             return true
         }
     }
