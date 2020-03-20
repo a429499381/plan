fbd556fb5c00ea79e501b0a666a65f753c266053
xu tao <429499381@qq.com>
2020-03-20 10:35:59 +0800

    给开始界面加背景01

--- scenePro/page/title.js
+++ scenePro/page/title.js
@@ -11,18 +11,18 @@ class Title extends Page {
             key: "KeyK",
         }
 
-        this.image = this.game.ui.imgPath(this.game.imgAll.bg1),
-            this.game.regEvent.register(this.title.key, this.update)
+        this.image = this.game.ui.imgPath(this.game.imgAll.bg1)
+        this.game.regEvent.register(this.title.key, this.update)
         this.draw(this.title.name)
 
     }
 
     draw(name, x = 120, y = 400) {
         this.game.ui.clearUi()
-        this.game.ui.context.drawImage(this.image, 0, 0);
+        this.game.ui.context.drawImage(this.image, 0, 0, 400, 760);
         // this.ui.context.drawImage(img.image, img.x, img.y,);
         this.game.ui.context.font = "20px Georgia";
-        this.game.ui.context.fillStyle = "red";
+        this.game.ui.context.fillStyle = "white";
         this.game.ui.context.fillText(name, x, y)
 
     }
--- scenePro/scene_game.js
+++ scenePro/scene_game.js
@@ -8,7 +8,7 @@ class SceneGame extends ScenePro {
         setup() {
             this.SceneGameTimeId = -1
             this.isgameOver = false
-            this.addOther(this.imgAll.bg2)
+            this.addOther(this.imgAll.bg)
             this.addOther(this.imgAll.yun0, this.Between(0, 50), this.Between(0, 100))
             this.enemy = new Enemy(this)
             this.player = new Player(this)
